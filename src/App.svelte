ed728b49b8159389f931419fb829917320ca0ffd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-12 11:58:14 +0530

    rendering from variables

--- src/App.svelte
+++ src/App.svelte
@@ -62,41 +62,16 @@ const onSearchHandler = () => {
 <h1>List of List</h1>
 <p>simplified version of workflowy.com or basic slack</p>
 <ul>
+	{#each outerList as outerListName}
 	<li>
-		Kotlin List
+		{outerListName}
 		<ul>
-			<li>kotlin js</li>
-			<li>kotliin classes</li>
-			<li>Kotlin collections</li>
-			<li>Kotlin Android</li>
-			<li>kotlin examples</li>
-			<li>Kotlin kones</li>
-			<li>Kotlin ktor</li>
-			<li>Kotlin kmongo</li>
+			{#each lldata[outerListName] as item}
+			<li>{item}</li>
+			{/each}
 		</ul>
 	</li>
-	<li>Java List
-		<ul>
-			<li>Java OOPS</li>
-			<li>Java collections</li>
-			<li>Java spring boot</li>
-		</ul>
-	</li>
-	<li>Svelte List
-		<ul>
-			<li>Svelte hello world</li>
-			<li>Dynamic Attributes</li>
-			<li>Styling</li>
-			<li>nested components</li>
-			<li>Html tags</li>
-			<li>Reactivity</li>
-			<li>Props</li>
-			<li>Logic</li>
-			<li>Events</li>
-			<li>Lifecycle</li>
-			<li>Store</li>
-		</ul>
-	</li>
+	{/each}
 </ul>
 </main>
 
